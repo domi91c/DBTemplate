79a8865eef899a55184aa020ac1a7fb769efce8b
portOdin <dominic.n@me.com>
2014-05-17 04:20:13 -0400

    so, i tried copying all the files from the devise book examples. got some sqlite3 constraint error. the projects accept other db's, like mysql and mongodb, so i'll probably try and install one of those, and run the files later, but for now i'm going to use this project, as it's working great, just doesn't have all the login features. but it has enough, so i'm going to move onto the more important stuff using this project. good night

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,32 @@
-class UsersController < Devise::SessionsController
+class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+
+	def new
+		@user = User.new
+	end
 	def create
-		super
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to posts_path, notice: 'User successfully added.'
+		else
+			render action: :new
+		end
+	end
+	def edit
 	end
 	def update
-		#edit here
+		if @user.update(user_params)
+			redirect_to posts_path, notice: 'Updated user information successfully.'
+		else
+			render action: 'edit'
+		end
+	end
+	private
+	def set_user
+		@user = User.find(params[:id])
+	end
+	def user_params
+		params
 	end
 end
