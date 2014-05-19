cf619ee63b591fd8395ba43e285cdc4f8d43668e
portOdin <dominic.n@me.com>
2014-05-19 01:34:12 -0400

    logout stopped working,
    "The action 'destroy' could not be found for UsersController"
    
    so, gonna check a previous branch and fix this
    
    profiles/index not working. going to checkout previoius.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,7 @@ class ProfilesController < ApplicationController
   # GET /profiles
   # GET /profiles.json
   def index
-    @profiles = Profile.all
+    @profile = Profile.all
   end
 
   # GET /profiles/1
